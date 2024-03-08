c6119dc24c68a5c6b8a4b7016e50b25b2dc77392
alzamer <mic.szendzielorz@gmail.com>
2024-03-08 21:08:26 +0100

    Added dark mode

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,3 +1,14 @@
+import style from './style.module.css';
+
 export default function Footer(){
-  return <h1>Hello, World!</h1>;
+  return <div className={style.container}>
+    <div className={style.column}>
+      <p>Link #1</p>
+      <p>Link #2</p>
+      <p>Link #3</p>
+      <p>Link #4</p>
+      <p>Link #5</p>
+      <p>Link #6</p>
+    </div>
+  </div>;
 }
--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,10 +1,34 @@
+'use client'
+
 import style from './style.module.css';
 import { MdDarkMode } from "react-icons/md";
+import { TiThMenu } from "react-icons/ti";
 import { MdOutlineDarkMode } from "react-icons/md";
-import { useTheme } from "next-themes";
+import Modal from 'react-modal';
+import { useState, useCallback } from 'react';
+import { signIn, signUp } from './actions'
+import { useTheme } from 'next-themes'
 
 export default function Header(){
-  const { theme, setTheme } = useTheme();
+  const [signInModal, setSignInModal] = useState(false);
+  const [signUpModal, setSignUpModal] = useState(false);
+  const { theme, setTheme } = useTheme()
+
+  const switchSignInModal = useCallback(() => {
+    setSignInModal(!signInModal);
+  }, [signInModal, setSignInModal]);
+
+  const switchTheme = useCallback(() => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }, [theme, setTheme]);
+
+  const handleSignIn = useCallback(async () => {
+    switchSignInModal();
+    await signIn("", "");
+  }, [switchSignInModal, signIn]);
+
+  Modal.setAppElement('body');
+
   return <div className={style.container}>
     <div className={style.left}>
       <span>MDNest</span>
@@ -13,11 +37,35 @@ export default function Header(){
 
     </div>
     <div className={style.right}>
-      <div className={style.darkMode}>
-        {theme === "dark" && <MdDarkMode/>}
+      <div className={style.darkMode} onClick={switchTheme}>
+        { theme === 'light' ? <MdDarkMode/> : <MdOutlineDarkMode/>}
       </div>
-      <p>Log In</p>
-      <p onClick={() => theme === 'light' ? setTheme('dark') : setTheme('light')}>Sign Up</p>
+      <p onClick={switchSignInModal}>Sign In</p>
+      <p onClick={() => setSignUpModal(true)}>Sign Up</p>
     </div>
+    <div className={style.rightSmall}>
+      <TiThMenu size="1.5em"/>
+    </div>
+    <Modal
+      isOpen={signInModal}
+      onAfterOpen={() => null}
+      onRequestClose={switchSignInModal}
+      className={style.modal}
+      contentLabel="Sign in modal">
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button formAction={handleSignIn}>Log in</button>
+      </form>
+    </Modal>
+    <Modal
+      isOpen={signUpModal}
+      onAfterOpen={() => null}
+      onRequestClose={() => setSignUpModal(false)}
+      className={style.modal}
+      contentLabel="Sign up modal">
+    </Modal>
   </div>;
 }
