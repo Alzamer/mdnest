995800495083f0c286c921e44697c47d68d6ded3
alzamer <mic.szendzielorz@gmail.com>
2024-03-15 17:51:39 +0100

    Added Content component

--- src/app/components/Content/index.tsx
+++ src/app/components/Content/index.tsx
@@ -1,7 +1,7 @@
 import style from './style.module.css'
 
 export default function Content(){
-  return <div>
+  return <div className={style.container}>
     <div>#1</div>
     <div>#2</div>
     <div>#3</div>
