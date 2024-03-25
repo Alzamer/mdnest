0240b213140399925d9e59161b2be5e39b2e1fc2
alzamer <mic.szendzielorz@gmail.com>
2024-03-25 22:31:53 +0100

    Added tabs component to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,51 @@
+'use client';
+
 import style from './style.module.css'
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { useState } from 'react';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 export default function Page(){
+  const [value, setValue] = useState(0);
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return <div className={style.container}>
     <div className={style.profile}>
       <div className={style.profileInfo}>
@@ -12,6 +57,20 @@ export default function Page(){
           <p className={style.date}>CREATED AT</p>
         </div>
         <button>Follow</button>
+      </div>
+      <div>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Notes" {...a11yProps(0)} />
+          <Tab label="Activity" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        Notes
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        Activity
+      </CustomTabPanel>
       </div>
     </div>
   </div>;
