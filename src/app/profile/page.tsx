f4307f53160fcac77ae2ee3514ad1f2f762269da
alzamer <mic.szendzielorz@gmail.com>
2024-03-25 22:10:39 +0100

    Added profile info component

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,17 @@ import style from './style.module.css'
 
 export default function Page(){
   return <div className={style.container}>
-		<h1>Hello, World!</h1>
+    <div className={style.profile}>
+      <div className={style.profileInfo}>
+        <div className={style.picture}>
+
+        </div>
+        <div>
+          <p className={style.username}>USERNAME</p>
+          <p className={style.date}>CREATED AT</p>
+        </div>
+        <button>Follow</button>
+      </div>
+    </div>
   </div>;
 }
