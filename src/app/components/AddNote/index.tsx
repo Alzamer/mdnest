d67cf1c68142ade166b7b5b33b520c482a69e71f
alzamer <mic.szendzielorz@gmail.com>
2024-03-27 22:58:40 +0100

    Added AddNote component

---
+++ src/app/components/AddNote/index.tsx
@@ -0,0 +1,7 @@
+import style from './style.module.css'
+
+export default function AddNote(){
+  return <div className={style.container}>
+    <b>+</b>
+  </div>;
+}
--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,14 +1,18 @@
 import style from './style.module.css';
+import AddNote from '../AddNote';
 
 export default function Footer(){
-  return <div className={style.container}>
-    <div className={style.column}>
-      <p>Link #1</p>
-      <p>Link #2</p>
-      <p>Link #3</p>
-      <p>Link #4</p>
-      <p>Link #5</p>
-      <p>Link #6</p>
+  return <>
+    <AddNote/>
+    <div className={style.container}>
+      <div className={style.column}>
+        <p>Link #1</p>
+        <p>Link #2</p>
+        <p>Link #3</p>
+        <p>Link #4</p>
+        <p>Link #5</p>
+        <p>Link #6</p>
+      </div>
     </div>
-  </div>;
+  </>;
 }
