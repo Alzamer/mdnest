eb09f86f37993b7609a8b5d640d3af2b14ea8e31
alzamer <mic.szendzielorz@gmail.com>
2024-03-28 20:03:03 +0100

    Added /create page

---
+++ src/app/create/page.tsx
@@ -0,0 +1,9 @@
+import style from './style.module.css';
+
+export default function Page(){
+  return <div className={style.container}>
+    <div className={style.profile}>
+      Create a page
+    </div>
+  </div>;
+}
