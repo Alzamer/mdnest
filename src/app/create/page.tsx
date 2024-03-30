82d13df94b720177125988d35330af26fc4c39c8
alzamer <mic.szendzielorz@gmail.com>
2024-03-30 21:55:26 +0100

    Added md editor to /create

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,9 +1,25 @@
+'use client';
+
 import style from './style.module.css';
+import { useState } from 'react';
+import { Input, Button } from 'semantic-ui-react'
+import MDEditor from '@uiw/react-md-editor';
 
 export default function Page(){
+  const [value, setValue] = useState("");
   return <div className={style.container}>
     <div className={style.profile}>
-      Create a page
+      <h1><Input placeholder='Title'/></h1>
+      <h1>
+        <MDEditor
+          value={value}
+          onChange={setValue}
+          height={600}
+          minHeight={600}
+          maxHeight={600}
+        />
+      </h1>
+      <Button primary>Add note</Button>
     </div>
   </div>;
 }
