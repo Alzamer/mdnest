b75c47775f27d68ccfe835dfcad7279134d36747
alzamer <mic.szendzielorz@gmail.com>
2024-04-07 14:42:32 +0200

    Populated note page

--- src/app/note/[id]/page.tsx
+++ src/app/note/[id]/page.tsx
@@ -1,3 +1,11 @@
+import styles from './style.module.css';
+
 export default function Page({ params } : { params : { id : number}}) {
-  return <p>Post: {params.id}</p>
+  return <div className={styles.container}>
+    <div className={styles.header}>TITLE</div>
+    <div className={styles.content}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie nisl nec arcu vulputate tincidunt. Nunc at vehicula erat, vitae blandit lectus. Vivamus dapibus interdum arcu, quis dapibus ante suscipit vel. Quisque ullamcorper sodales sapien, sit amet varius orci sodales a. Suspendisse ultricies diam at pellentesque condimentum. Donec sed nisi tellus. Cras lacinia, quam vel porta blandit, risus elit ultricies lorem, volutpat euismod ante eros ut velit. Sed congue vel lorem eget tristique. Sed vel aliquet dui. Aliquam erat volutpat. Integer ornare nisi turpis, nec eleifend est condimentum eget. Aliquam erat volutpat. Fusce eu porta sem, non facilisis neque. Ut posuere, odio nec fringilla gravida, orci mauris convallis nulla, sit amet sodales mauris libero id quam. Aenean faucibus nulla sit amet pharetra aliquam. 
+    </div>
+    <div className={styles.comments}>COMMENTS</div>
+  </div>;
 }
