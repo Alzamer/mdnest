7fd5072b93659af12fba969ce482221f9916abec
alzamer <mic.szendzielorz@gmail.com>
2024-04-14 19:10:18 +0200

    Added Suspense to /profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,14 @@
 import style from './style.module.css';
 import ProfileHeader from '../components/ProfileHeader';
 import TabsWrapper from '../components/TabsWrapper';
+import { Suspense } from 'react';
 
 export default function Page(){
   return <div className={style.flexContainer}>
     <div className={style.container}>
-      <ProfileHeader/>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <ProfileHeader/>
+      </Suspense>      
       <TabsWrapper/>
     </div>
   </div>;
