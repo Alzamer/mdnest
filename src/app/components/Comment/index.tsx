3995e11a965c392784295d4dcfd929e8c635d064
alzamer <mic.szendzielorz@gmail.com>
2024-04-16 22:03:28 +0200

    Added overflow to comments

--- src/app/components/Comment/index.tsx
+++ src/app/components/Comment/index.tsx
@@ -1,9 +1,26 @@
+'use client';
+import { useEffect, useState } from 'react';
 import styles from './style.module.css';
+import { createClient } from '../../../../utils/supabase/client';
+
+const supabase = createClient();
 
 export default function Comment({ author, comment, createdAt} : { author: string, comment: string, createdAt: string}) {
+  const [commentAuthor, setCommentAuthor] = useState('');
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await supabase
+      .from('profiles')
+      .select('username')
+      .match({
+        id: author
+      })
+      setCommentAuthor(data![0].username);
+    })()
+  }, []);
+
   return <div className={styles.container}>
-    <p>{ author }</p>
+    <p>{ commentAuthor ? commentAuthor : <span>Loading...</span> } { createdAt.slice(0, 10) }</p>
     <p>{ comment }</p>
-    <p>{ createdAt.slice(0, 10) }</p>
   </div>;
 }
