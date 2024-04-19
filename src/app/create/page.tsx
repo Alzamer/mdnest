9fb0e378ae05a1a193d752dfb30260b0e86b185d
alzamer <mic.szendzielorz@gmail.com>
2024-04-19 18:23:20 +0200

    Hide superfluois scrollbars

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -4,24 +4,59 @@ import style from './style.module.css';
 import { useState } from 'react';
 import { Input, Button } from 'semantic-ui-react'
 import MDEditor from '@uiw/react-md-editor';
+import { useRouter } from 'next/navigation';
+import { createClient } from '../../../utils/supabase/client';
+
+const supabase = createClient();
 
 export default function Page() {
-  const [value, setValue] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const router = useRouter();
+
+  const addNote = async () => {
+    const { data: { user } } = await supabase.auth.getUser();
+    const { error } = await supabase
+      .from('notes')
+      .insert({
+        title: title,
+        author: user!?.id,
+        content: content
+      });
+
+    if (error)
+      throw error;
+
+    router.push(`/`);
+  };
+
+  const handleSubmit = () => {
+    if (title === "" || content === "")
+      return;
+
+    addNote();
+  };
+
   return <div className={style.container}>
     <div className={style.profile} data-color-mode="light">
       <h1>
-        <Input placeholder='Title' />
+        <Input placeholder='Title'
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
       </h1>
       <h1>
         <MDEditor
-          value={value}
-          onChange={setValue as () => void}
+          value={content}
+          onChange={setContent as () => void}
           height={600}
           minHeight={600}
           maxHeight={600}
         />
       </h1>
-      <Button primary>Add note</Button>
+      <Button primary onClick={handleSubmit}>
+        Add note
+      </Button>
     </div>
   </div>;
 }
