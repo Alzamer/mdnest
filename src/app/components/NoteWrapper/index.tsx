5a39356e5ef9176e6bb54a39e24c6a18f9b24160
alzamer <mic.szendzielorz@gmail.com>
2024-04-26 20:15:24 +0200

    Migrated to pnpm

--- src/app/components/NoteWrapper/index.tsx
+++ src/app/components/NoteWrapper/index.tsx
@@ -1,12 +1,11 @@
-'use server';
-
 import styles from './style.module.css';
 import { marked } from 'marked';
-import { createClient} from '../../../../utils/supabase/server';
+import { createClient } from '../../../../utils/supabase/server';
 import parse from 'html-react-parser';
 
 export default async function NoteWrapper({ uuid }: { uuid: string }) {
   const supabase = createClient();
+  
   const { data, error } = await supabase
     .from('notes')
     .select()
