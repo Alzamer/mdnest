bcf5a2f9edc816f32dd1a535667506526513f2eb
alzamer <mic.szendzielorz@gmail.com>
2024-05-11 10:42:22 +0200

    Added TailSpinWrapper

---
+++ src/app/components/TailSpinWrapper/index.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { TailSpin } from "react-loader-spinner";
+
+export default function TailSpinWrapper(){
+    return <TailSpin
+    visible={true}
+    height="80"
+    width="80"
+    color="#4fa94d"
+    ariaLabel="tail-spin-loading"
+    radius="1"
+    wrapperStyle={{}}
+    wrapperClass=""
+  />
+}
--- src/app/note/[id]/page.tsx
+++ src/app/note/[id]/page.tsx
@@ -1,13 +1,14 @@
 import styles from "./style.module.css";
 import Comments from "@/app/components/Comments";
 import NoteWrapper from "@/app/components/NoteWrapper";
+import TailSpinWrapper from "@/app/components/TailSpinWrapper";
 import { Suspense } from "react";
 
 export default function Page({ params }: { params: { id: string } }) {
   return (
     <div className={styles.container}>
       <Comments id={params.id} uuid={params.id}>
-        <Suspense fallback={<h1>Loading...</h1>}>
+        <Suspense fallback={<TailSpinWrapper/>}>
           <NoteWrapper uuid={params.id} />
         </Suspense>
       </Comments>
