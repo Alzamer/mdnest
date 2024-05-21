929da0d9384096dc0f8d236449c6decbac44e497
alzamer <mic.szendzielorz@gmail.com>
2024-05-21 11:40:44 +0200

    Added downvote

--- src/app/components/Comments/index.tsx
+++ src/app/components/Comments/index.tsx
@@ -8,6 +8,7 @@ import { ToastContainer, toast } from "react-toastify";
 import { Database } from "@/app/types/supabase";
 import "react-toastify/dist/ReactToastify.css";
 import useUpvote from "../../../../utils/hooks/useUpvote";
+import useDownvote from "../../../../utils/hooks/useDownvote";
 import useGetCurrentUser from "../../../../utils/hooks/useGetCurrentUser";
 import { Session } from "@supabase/supabase-js";
 
@@ -22,12 +23,16 @@ export default function Comments({
   const [comments, setComments] = useState<
     Database["public"]["Tables"]["comment"]["Row"][]
   >([]);
-  const [pUuid, setpUuid] = useState<string | null>(null);
-  const [pUser, setpUser] = useState<Session | null>(null);
-  const [forceChange, setForceChange] = useState(false);
+  const [pUuidUpvote, setpUuidUpvote] = useState<string | null>(null);
+  const [pUuidDownvote, setpUuidDownvote] = useState<string | null>(null);
+  const [pUserUpvote, setpUserUpvote] = useState<Session | null>(null);
+  const [pUserDownvote, setpUserDownvote] = useState<Session | null>(null);  
+  const [forceUpvoteChange, setForceUpvoteChange] = useState(false);
+  const [forceDownvoteChange, setForceDownvoteChange] = useState(false);
 
   const { user } = useGetCurrentUser();
-  const { error, alreadyUpvoted } = useUpvote(pUuid, pUser, forceChange);
+  const { error, alreadyUpvoted } = useUpvote(pUuidUpvote, pUserUpvote, forceUpvoteChange);
+  const { error: errorDownvote, alreadyDownvoted } = useDownvote(pUuidDownvote, pUserDownvote, forceDownvoteChange);
 
   const notify = (msg: string) => toast(msg);
 
@@ -42,10 +47,27 @@ export default function Comments({
       notify("Upvoted note!");
   }, [alreadyUpvoted])
 
+  useEffect(() => {
+    if(alreadyDownvoted === null)
+      return;
+    else if(errorDownvote)
+      notify(errorDownvote);
+    else if(alreadyDownvoted)
+      notify("Already downvoted!");
+    else if (!alreadyDownvoted)
+      notify("Downvoted note!");
+  }, [alreadyDownvoted])
+
   const handleUpvote = () => {
-    setpUuid(uuid);
-    setpUser(user);
-    setForceChange(!forceChange);
+    setpUuidUpvote(uuid);
+    setpUserUpvote(user);
+    setForceUpvoteChange(!forceUpvoteChange);
+  }
+
+  const handleDownvote = () => {
+    setpUuidDownvote(uuid);
+    setpUserDownvote(user);
+    setForceDownvoteChange(!forceDownvoteChange);
   }
 
   return (
@@ -53,7 +75,7 @@ export default function Comments({
       <div className={style.contentContainer}>
         <div className={style.thumbs}>
           <span onClick={() => handleUpvote()}>👍</span>
-          <span>👎</span>
+          <span onClick={() => handleDownvote()}>👎</span>
         </div>
         <div className={style.content}>{children}</div>
       </div>
--- utils/hooks/useDownvote.ts
+++ utils/hooks/useDownvote.ts
@@ -1,5 +1,111 @@
+
+import { Session } from "@supabase/supabase-js";
 import supabase from "../../utils/supabase/client";
+import { useState, useEffect } from "react";
 
-export default function useDownvote(id: string){
+export default function useDownvote(id: string | null, accessToken: Session | null, forceChange: boolean) {
+  const [isError, setIsError] = useState('');
+  const [alreadyDownvoted, setAlreadyDownvoted] = useState<boolean | null>(null);
 
+  async function checkIfDownvoted() {
+    const { data, error } = await supabase
+      .from('profiles')
+      .select()
+      .match({
+        id: accessToken?.user.id
+      });
+
+    if (error) {
+      setIsError(`Error: ${error.code} ${error.message}`);
+      return;
+    }
+
+    if (data[0].downvoted !== null && id !== null && data[0].downvoted.includes(id))
+      setAlreadyDownvoted(true);
+    else
+      setAlreadyDownvoted(false);
+  }
+
+  async function updateProfile() {
+    const { data, error } = await supabase
+      .from('profiles')
+      .select()
+      .match({
+        id: accessToken?.user.id
+      });
+
+    if (error) {
+      setIsError(`Error: ${error.code} ${error.message}`);
+      return;
+    }
+
+    const { error: updateError } = await supabase
+      .from('profiles')
+      .update({
+        ...data[0],
+        downvoted: data[0].downvoted ?
+          [...data[0].downvoted, id!]
+          : [id!]
+      })
+      .match({
+        id: accessToken?.user.id
+      });
+
+    if (updateError) {
+      setIsError(`Error: ${updateError.code} ${updateError.message}`);
+      return;
+    }
+  };
+
+  async function updateNoteVoteCounter() {
+    const { data, error } = await supabase
+      .from('note')
+      .select()
+      .match({
+        id: id
+      });
+
+    if (error) {
+      setIsError(`Error: ${error.code} ${error.message}`);
+      return;
+    }
+
+    const { error: updateError } = await supabase
+      .from('note')
+      .update({
+        ...data[0],
+        upvotes: data[0].upvotes + 1
+      })
+      .match({
+        id: id
+      });
+
+    if (updateError) {
+      setIsError(`Error: ${updateError.code} ${updateError.message}`);
+      return;
+    }
+  }
+
+  useEffect(() => {
+    if (id === null || accessToken === null)
+      return;
+
+    checkIfDownvoted();
+  }, [id, accessToken, forceChange]);
+
+  useEffect(() => {
+    if(alreadyDownvoted === null)
+      return;
+
+    if(!alreadyDownvoted){
+      updateProfile();
+      updateNoteVoteCounter();
+    }
+
+  }, [alreadyDownvoted]);
+
+  return {
+    error: isError,
+    alreadyDownvoted: alreadyDownvoted
+  }
 };
